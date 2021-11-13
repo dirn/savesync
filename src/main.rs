07d66491cc7989d7ed1da0a3b4467f2d13bb6478
Andy Dirnberger <andy.dirnberger@gmail.com>
2021-11-13 16:46:43 -0500

    Store source and destination as paths, not strings

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 use std::env;
+use std::path::PathBuf;
 use std::process;
 
 #[derive(Debug)]
 struct Config {
-    src: String,
-    dest: String,
+    src: PathBuf,
+    dest: PathBuf,
 }
 
 fn load_configuration_from_environment() -> Result<Config, String> {
     let src = match env::var("RETRO_SAVES") {
-        Ok(val) => val,
+        Ok(val) => PathBuf::from(val),
         Err(e) => return Err(format!("RETRO_SAVES: {}", e)),
     };
 
     let dest = match env::var("RETRO_GAMES") {
-        Ok(val) => val,
+        Ok(val) => PathBuf::from(val),
         Err(e) => return Err(format!("RETRO_GAMES: {}", e)),
     };
 
