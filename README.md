14c0e546f998e275f3a7e201e86eae283096347f
Andy Dirnberger <andy.dirnberger@gmail.com>
2021-11-13 15:01:08 -0500

    Check for source environment variable

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Synchronize game saves.
 ## Configuration
 
 `RETRO_GAMES`: The location to which to synchronize the saves.
+`RETRO_SAVES`: The location from which to synchronize the saves.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::env;
 
 fn main() {
+    match env::var("RETRO_SAVES") {
+        Ok(val) => println!("RETRO_SAVES: {}", val),
+        Err(e) => println!("RETRO_SAVES: {}", e),
+    }
+
     match env::var("RETRO_GAMES") {
         Ok(val) => println!("RETRO_GAMES: {}", val),
         Err(e) => println!("RETRO_GAMES: {}", e),
